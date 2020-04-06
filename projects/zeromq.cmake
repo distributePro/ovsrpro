77cb192d54c2235e897fc0809f058e37ec620329
brobeson <brobeson@users.noreply.github.com>
2020-04-05 22:12:07 -0600

    Clean up the ZeroMQ CMake file
    - Lines are limited to 80 characters.
    - Commands are more readable.
    - I added CMake command line options to the ZeroMQ build. Extras like
    tests are not built now.
    
    issue brobeson/ovsrpro#1

--- projects/zeromq.cmake
+++ projects/zeromq.cmake
@@ -1,9 +1,5 @@
-########################################
-# zeromq
-########################################
 xpProOption(zeromq)
-set(ZEROMQ_VERSION 4.2.3)
-set(ZEROMQ_SRC_PATH ${CMAKE_BINARY_DIR}/xpbase/Source/zeromq)
+set(VERSION 4.2.3)
 set(REPO https://github.com/zeromq/libzmq)
 set(PRO_ZEROMQ
   NAME zeromq
@@ -11,26 +7,31 @@ set(PRO_ZEROMQ
   LICENSE "LGPL" ${REPO}/blob/master/README.md "LGPL v3 (See License Section)"
   DESC "ZeroMQ is a lightweight messaging kernel that extends standard socket interfaces with more powerful features."
   REPO "repo" ${REPO} "ZeroMQ repo on github"
-  VER ${ZEROMQ_VERSION}
-  DLURL ${REPO}/archive/v${ZEROMQ_VERSION}.tar.gz
+  VER ${VERSION}
+  DLURL ${REPO}/archive/v${VERSION}.tar.gz
   DLMD5 ad07f71105fc914e1937683d33b4ee8d
-  DLNAME libzmq-${ZEROMQ_VERSION}.tar.gz
+  DLNAME libzmq-${VERSION}.tar.gz
 )
 
 function(build_zeromq)
   if (NOT (XP_DEFAULT OR XP_PRO_ZEROMQ))
     return()
   endif()
-  configure_file(${PRO_DIR}/use/useop-zeromq-config.cmake ${STAGE_DIR}/share/cmake/
-    @ONLY NEWLINE_STYLE LF
+  configure_file(
+    "${PRO_DIR}/use/useop-zeromq-config.cmake"
+    "${STAGE_DIR}/share/cmake/"
+    @ONLY
+    NEWLINE_STYLE LF
   )
-
   xpSetPostfix()
-  set(XP_CONFIGURE
+  set(
+    XP_CONFIGURE
     -DCMAKE_DEBUG_POSTFIX=${CMAKE_DEBUG_POSTFIX}
     -DCMAKE_INSTALL_LIBDIR=lib
     -DCMAKE_INSTALL_INCLUDEDIR=include/zeromq
-    )
+    -DWITH_PERF_TOOL=off
+    -DZMQ_BUILD_TESTS=off
+    -DENABLE_CPACK=off
+  )
   xpCmakeBuild(zeromq "" "${XP_CONFIGURE}")
-
 endfunction()
